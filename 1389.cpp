03af0368324b79da1ed7ae7bcddadbd6aeaef829
sonhl0723 <hongil@hongsseuui-MacBookAir.local>
2020-08-16 14:15:00 +0900

    20200816

--- 1389.cpp
+++ 1389.cpp
@@ -3,18 +3,31 @@
 #include <queue>
 #include <vector>
 #include <algorithm>
+#include <cstring>
 
 using namespace std;
 
 int N, M;
 bool check[101][101];
-int ans;
 queue<int> q;
-int sum[101];
+int step[101];
 
-void bfs(int index){
-  for(int i = 1; i < N+1; i++){
-    q.push(i);
+void bfs(int start){
+  q.push(start);
+  step[start] = step[start] + 1;
+  while(!q.empty()){
+    int q_size = q.size();
+    while(q_size--){
+      int current = q.front();
+      q.pop();
+      for(int i = 1; i < N+1; i++){
+        if(check[current][i] != 1 || step[i] >= 0){
+          continue;
+        }
+        q.push(i);
+        step[i] = step[current] + 1;
+      }
+    }
   }
 }
 
@@ -24,6 +37,8 @@ int main(void){
   cout.tie(NULL);
 
   cin >> N >> M;
+
+  memset(check, 0, sizeof(check));
 
   for(int i = 0; i < M; i++){
     int a, b;
@@ -35,10 +50,23 @@ int main(void){
     }
   }
 
-  for(int i = 1; i < N+1; i++){
-    memset(sum, -1, sizeof(sum));
-    bfs(i);
+  int min = 1000000000;
+  int real_ans = 0;
 
+  for(int i = 1; i < N+1; i++){
+    memset(step, -1, sizeof(step));
+    bfs(i);
+    int sum = 0;
+    for(int j = 1; j < N+1; j++){
+      sum = sum + step[j];
+    }
+    if(sum < min){
+      min = sum;
+      real_ans = i;
+    }
   }
+
+  cout << real_ans << '\n';
+
   return 0;
 }
