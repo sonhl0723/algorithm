a60dc1c28a105d6b32a865954227c3f6f99ad0fa
sonhl0723 <hongil@hongsseuui-MacBookAir.local>
2020-08-21 22:40:15 +0900

    20200821

--- 11660.cpp
+++ 11660.cpp
@@ -1,4 +1,4 @@
-#include <iostrea>
+#include <iostream>
 #include <vector>
 
 using namespace std;
@@ -28,13 +28,30 @@ int main(void){
 
   while(M--){
     int x1, y1, x2, y2;
-    cin >> x1 >> y1 >> x2 >> y2;
+    cin >> y1 >> x1 >> y2 >> x2;
 
-    int ans;
+    int ans = 0;
 
-    for(int i = x1-1; i < x2 - x1 + 1; i++){
-      int ans += v[y1-1][i]
+    for(int i = 0; i < y2-y1+1; i++){
+      if(x1 == x2){
+        if(x1 == 1){
+          ans = ans + v[y1 - 1 + i][x2-1];
+        }
+        else{
+          ans = ans + (v[y1 - 1 + i][x2-1] - v[y1 - 1 + i][x2-2]);
+        }
+      }
+      else{
+        if(x1 == 1){
+          ans = ans + v[y1 - 1 + i][x2-1];
+        }
+        else{
+          ans = ans + (v[y1 - 1 + i][x2-1] - v[y1 - 1 + i][x1-2]);
+        }
+      }
     }
+
+    cout << ans << '\n';
   }
 
   return 0;
