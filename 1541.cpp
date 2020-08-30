03e8b4837c4b59948b98f6e713d902230bda2e31
sonhl0723 <hongil@hongsseuui-MacBookAir.local>
2020-08-30 15:41:34 +0900

    20200830

--- 1541.cpp
+++ 1541.cpp
@@ -1,17 +1,77 @@
 #include <iostream>
+#include <string>
 #include <queue>
+#include <cmath>
 
 using namespace std;
-
-#define pb push_back
-#define mp make_pair
 
 int main(void){
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
   cout.tie(NULL);
 
-  queue<int> q;
+  string Test;
+  getline(cin, Test);
+  int sum = 0;
+  queue<int> q1, q2;
+  int ans = 0;
+
+  for(int i = 0; i < Test.length(); i++){
+    if(Test[i] == '-'){
+      while(!q2.empty()){
+        sum = sum + q2.front();
+        q2.pop();
+      }
+      q1.push(sum);
+      sum = 0;
+      continue;
+    }
+    else if(Test[i] == '+'){
+      q2.push(sum);
+      sum = 0;
+      continue;
+    }
+
+    int a = Test[i] - '0';
+    sum = (sum*10) + a;
+
+    if(i == Test.length()-1){
+      q2.push(sum);
+      sum = 0;
+      while(!q2.empty()){
+        sum = sum + q2.front();
+        q2.pop();
+      }
+      q1.push(sum);
+    }
+
+    // cout << sum << " ";
+  }
+
+  if(q1.empty()){
+    sum = 0;
+    while(!q2.empty()){
+      sum = sum + q2.front();
+      q2.pop();
+    }
+    ans = sum;
+  }
+
+  int check_f = 0;
+
+  while(!q1.empty()){
+    if(check_f == 0){
+      ans = q1.front();
+      q1.pop();
+      check_f++;
+      continue;
+    }
+    ans = ans - q1.front();
+    q1.pop();
+  }
+
+  cout << ans << '\n';
+
 
   return 0;
 }
