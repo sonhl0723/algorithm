9a02e0ac2ee19fbdf551645a2a950c3f5c7756d3
sonhl0723 <sonhl0723@naver.com>
2022-07-03 00:42:44 +0900

    투에-모스 문자열

--- BOJ_restart/Divide and Conquer/18222.py
+++ BOJ_restart/Divide and Conquer/18222.py
@@ -1,7 +1,13 @@
 # -*- coding: utf-8 -*-
-# 
+# 최대 10^18이 입력으로 들어오기 때문에 시간을 지키려면 점화식을 이용해서 풀어야함
 
-import sys
+k = int(input())-1
 
-k = int(input())
-X = "01"
+def rec(idx):
+    if idx == 0: return 0
+    elif idx == 1: return 1
+    else:
+        if idx%2==0: return rec(idx//2)
+        else: return 1-rec(idx//2)
+
+print(rec(k))
